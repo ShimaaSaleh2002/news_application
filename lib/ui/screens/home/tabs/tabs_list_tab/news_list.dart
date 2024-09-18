da50710486234c0fdbc5ac9c2c1982faf4c801b2
shimaaa_saleh <123778598+ShimaaSaleh2002@users.noreply.github.com>
2024-09-18 21:45:20 +0300

    nothing applied to master

--- lib/ui/screens/home/tabs/tabs_list_tab/news_list.dart
+++ lib/ui/screens/home/tabs/tabs_list_tab/news_list.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/ui/screens/home/tabs/tabs_list_tab/news_item.dart';
-import 'package:news_app/ui/screens/home/tabs/tabs_list_tab/news_list_view_model.dart';
-import 'package:provider/provider.dart';
 import '../../../../../data/api_manager.dart';
 import '../../../../../data/models/article.dart';
 import '../../../../../data/models/articles_response.dart';
@@ -19,36 +17,27 @@ class NewsList extends StatefulWidget {
 }
 
 class _NewsListState extends State<NewsList> {
-  NewsListViewModel viewModel =NewsListViewModel();
+
 
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    viewModel.getArticles(widget.source.id??"");
-  }
-  @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => viewModel,
-      /*
-      * why consumer not interface ?
-      * consumer : as i redraw the screen in multiple cases
-      * interface : action above my widget[alert dialog - snackBar - specific toast]*/
-      child: Consumer<NewsListViewModel>(
-          builder: (context, viewModel, child) {
-            if(viewModel.errorMessage != null){
-              return ErrorView(error: viewModel.errorMessage, onRetryClick: (){
-                viewModel.getArticles(widget.source.id!);
+    return FutureBuilder<ArticlesResponse?>(
+        future: ApiManager.getArticles(widget.source.id!),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return ErrorView(
+                error: snapshot.error.toString(), onRetryClick: () {
+              ApiManager.getArticles(widget.source.id!);
+              setState(() {
+
               });
-            }
-            if(viewModel.articlesList != null){
-              return buildNewsList(viewModel.articlesList!);
-            }else{
-              return const LoadingView();
-            }
-          },),
-    );
+            });
+          } else if (snapshot.hasData) {
+            return buildNewsList(snapshot.data!.articles!);
+          } else {
+            return const LoadingView();
+          }
+        });
   }
 
   Widget buildNewsList(List<Article> list) {
@@ -59,45 +48,4 @@ class _NewsListState extends State<NewsList> {
   }
 }
 
-// Widget mapArticleToNewsWidget(BuildContext context, Article article) {
-//   return Column(
-//     children: [
-//       CachedNetworkImage(
-//         imageUrl: article.urlToImage ?? "",
-//         placeholder: (context, url) => const LoadingView(),
-//         errorWidget: (context, url, error) =>const  Icon(Icons.error),
-//         height: MediaQuery.of(context).size.height * .25,
-//       ),
-//       Text(article.source?.name ?? ""),
-//       Text(article.title ?? ""),
-//       Text(formatTimeAgo(article.publishedAt.toString() ?? ""))
-//     ],
-//   );
-// }
-//
-// String formatTimeAgo(String dateString) {
-//   // Parse the ISO 8601 date string
-//   DateTime dateTime = DateTime.parse(dateString);
-//
-//   // Return the formatted time ago string
-//   return timeago.format(dateTime);
-// }
 
-
-// FutureBuilder<ArticlesResponse?>(
-// future: ApiManager.getArticles(widget.source.id!),
-// builder: (context, snapshot) {
-// if (snapshot.hasError) {
-// return ErrorView(
-// error: snapshot.error.toString(), onRetryClick: () {
-// ApiManager.getArticles(widget.source.id!);
-// setState(() {
-//
-// });
-// });
-// } else if (snapshot.hasData) {
-// return buildNewsList(snapshot.data!.articles!);
-// } else {
-// return const LoadingView();
-// }
-// });
--- lib/ui/screens/home/tabs/tabs_list_tab/tabs_list.dart
+++ lib/ui/screens/home/tabs/tabs_list_tab/tabs_list.dart
@@ -5,67 +5,54 @@ import 'package:news_app/data/models/source.dart';
 import 'package:news_app/data/models/sources_response.dart';
 import 'package:news_app/ui/screens/home/tabs/tabs_list_tab/news_list.dart';
 import 'package:news_app/ui/screens/home/tabs/tabs_list_tab/tab_item.dart';
-import 'package:news_app/ui/screens/home/tabs/tabs_list_tab/tabs_list_view_model.dart';
 import 'package:news_app/ui/widgets/error_view.dart';
 import 'package:news_app/ui/widgets/loading_view.dart';
-import 'package:provider/provider.dart';
 
 class TabsList extends StatefulWidget {
   Category category;
-  TabsList({required this.category,super.key});
+  TabsList({required this.category, super.key});
 
   @override
   State<TabsList> createState() => _TabsListState();
 }
 
 class _TabsListState extends State<TabsList> {
-  TabsListViewModel viewModel = TabsListViewModel();
   int selectedTabIndex = 0;
 
-  /*
-  * to call the function to get data:
-  * 1 - inside vm constructor
-  * 2 - inside init state
-  * */
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    viewModel.getSources(widget.category.backEndId);
-  }
+
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => viewModel, //creates obj of the vm
-      child: Consumer<TabsListViewModel>( //listens to viewModel
-
-        //don't wanna redraw the screen
-        //const child button icon But listview inside consumer..so rebuild won't
-        //affect children ..better performance[like const keyword before constructor in stless]
-          builder: (context, viewModel, child) {
-            //consumer can check anything inside viewModel as it listens
-            //helps me draw widgets depending on vm data
-            if(viewModel.errorMessage != null){
-              return ErrorView(error: viewModel.errorMessage, onRetryClick: (){
-                viewModel.getSources(widget.category.backEndId);
+    return FutureBuilder<SourcesResponse?>(
+      future: ApiManager.getSources(
+          widget.category.backEndId) //calls z function that returns future
+      ,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+        //client error ..didn't manage to parse as i've no response..didn't reach the server [catch]
+        //no internet connection
+          return ErrorView(
+              error: snapshot.error.toString(),
+              onRetryClick: () {
+                ApiManager.getSources(widget.category.backEndId);
+                setState(() {});
               });
-            }
-            if(viewModel.sourceList == null){
-              return const LoadingView();
-            }else{
-              return buildTabsList(viewModel.sourceList!);//i'm sure ive a list
-            }
-          },)
+        } else if (snapshot.hasData) {
+          return buildTabsList(snapshot.data!.sources!);
+        } else {
+          return const LoadingView();
+        }
+      },
     );
   }
 
   Widget buildTabsList(List<Source>? sources) {
     List<Widget> tabs = sources!
-        .map((source) =>
-        TabItem(sourceName: source.name ?? "",isSelected: sources?.indexOf(source) == selectedTabIndex))
+        .map((source) => TabItem(
+            sourceName: source.name ?? "",
+            isSelected: sources?.indexOf(source) == selectedTabIndex))
         .toList();
     List<Widget> tabsContent =
-    sources.map((source) => NewsList(source: source)).toList();
+        sources.map((source) => NewsList(source: source)).toList();
     return DefaultTabController(
       length: sources!.length,
       child: Column(
@@ -88,37 +75,3 @@ class _TabsListState extends State<TabsList> {
     );
   }
 }
-
-// Widget mapSourceToTab(Source source, bool isSelected) {
-//   return Container(
-//     padding: const EdgeInsets.all(12),
-//     decoration: BoxDecoration(
-//         borderRadius: BorderRadius.circular(22),
-//         border: Border.all(color: Colors.blue, width: 3),
-//         color: isSelected ? Colors.blue : Colors.white),
-//     child: Text(
-//       source.name ?? "",
-//       style: TextStyle(color: isSelected ? Colors.white : Colors.blue),
-//     ),
-//   );
-// }
-
-
-// FutureBuilder<SourcesResponse?>(
-// future: ApiManager.getSources(widget.category.backEndId) //calls z function that returns future
-// , builder: (context, snapshot) {
-// if (snapshot.hasError) {
-// //client error ..didn't manage to parse as i've no response..didn't reach the server [catch]
-// //no internet connection
-// return ErrorView(error: snapshot.error.toString(), onRetryClick: () {
-// ApiManager.getSources(widget.category.backEndId);
-// setState(() {
-// });
-// });
-// }else if (snapshot.hasData) {
-// return buildTabsList(snapshot.data!.sources!);
-// }else {
-// return const LoadingView();
-// }
-// },
-// ),
